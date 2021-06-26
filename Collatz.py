54a69b9022da17b30a472aab999ba5501d7d832d
k0128hj@naver.com <k0128hj@naver.com>
2021-06-26 12:30:57 +0900

    0626_study

---
+++ Collatz.py
@@ -0,0 +1,25 @@
+def solution(num):
+    answer = 0
+    
+    if num==1:
+        return 0
+    
+    while True:
+        if num%2==0:
+            num=num//2
+            
+            
+        else:
+            num=num*3+1
+        
+        answer+=1
+        
+        if num==1:
+            break
+        
+        if answer>=500:
+            answer=-1
+            break
+        
+            
+    return answer
