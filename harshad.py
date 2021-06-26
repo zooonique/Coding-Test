f7b9124fa24257a6edfd849c2389617d13d58b2c
k0128hj@naver.com <k0128hj@naver.com>
2021-06-26 12:28:17 +0900

    0626_study

---
+++ harshad.py
@@ -0,0 +1,14 @@
+def solution(x):
+    answer = True
+    num = 0
+    
+    for i in str(x):
+        num+=int(i)
+    
+    if x%num==0:
+        answer = True
+    
+    else:
+        answer = False
+    
+    return answer
